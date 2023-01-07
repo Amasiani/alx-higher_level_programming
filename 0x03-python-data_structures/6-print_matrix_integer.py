1d0cac1f919a187c998bb2191f8cc2bf8afd7ab6
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-07 14:04:44 -0800

    print matrix integer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrox=[[]]):
     for x in matrix:
         s = ""
         for y in x:
-            print("{:s}{:d}".format(s, y,) end='')
+            print("{:s}{:d}".format(s, y), end='')
             s = " "
         print()
