770ef3b265c22646564adb85c79dfca59e88b5e1
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-09 19:07:21 -0800

    remove "c" and "C" from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     string = ""
     for i in range(len(my_string)):
         if (my_string[i] != 'c') and (my_string[i] != 'C'):
-            string += my_string
+            string += my_string[i]
     return (string)
