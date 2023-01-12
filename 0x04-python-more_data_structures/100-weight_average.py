c00002e8923dbe063a0cf45f1f5da99ed59c8986
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-11 20:59:18 -0800

    return weighted average of all integer value in a tuple

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is None or len(my_list) == 0:
+        return(0)
+    a = 0
+    b = 0
+    for i in range(len(my_list)):
+        for j in range(len(my_list]):
+            a = a + (my_list[i][j] * my_list[i][j+1])
+            b = b + my_list[i][j+1]
+            break
+    return(a / b)
