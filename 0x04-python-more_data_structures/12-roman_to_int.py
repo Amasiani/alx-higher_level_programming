8e841dc29715c646a65a14bdcb41cdd7d20dc271
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-11 20:47:59 -0800

    convert roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 50, 'M': 1000}
     a = 0
     for k in range(len(roman_string)):
-        if k > 0 and roman(roman_string[k] > roman[roman_string[k-1]]):
+        if k > 0 and roman[roman_string[k]] > roman[roman_string[k-1]]:
             a += roman[roman_string[k]] - 2 * roman[roman_string[k-1]]
         else:
             a += roman[roman_string[k]]
