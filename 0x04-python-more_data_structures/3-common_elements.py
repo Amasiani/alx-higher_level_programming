b9a17d9ce94e88d8ad5d555a7b327abddfa5f657
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-11 19:40:28 -0800

    return common items in a two set()

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    # Creating an empty list.
+    # Empty curl brace {will make a empty dictionary in python.
+    # To make a emptuy set we use set() function without argunents
+    #
+    # a = {}
+    # a = set()
+    #
+    return(set_1 & set_2)
