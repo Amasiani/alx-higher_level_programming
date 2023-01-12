b55f475bf81ee92fcf575876d186d0f36601d839
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-11 19:26:08 -0800

    add unique items in a list only onces

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    a = 0
+    items_list = dict() #create an empty dictionary
+    for i in range(len(my_list)):
+        if (my_list not in items_list.keys()): #if  not in the item list
+            items_list[my_list[i]] = my_list[i] #it will be added
+            a += my_list[i]
+    return(a)
