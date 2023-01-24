203991967f6e87827d9f2e629fc8d8eb9b0fca4d
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-23 20:44:03 -0800

    101-safe_function task correction

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,5 +7,7 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
     except Exception as i:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        print("Exception: {}".format(i), file=sys.stderr)
         return (None)
+    else:
+        return (result)
