f89115d5ee77bedcc6b88c72141eff4ac1255044
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-24 06:24:58 -0800

    first commit 1-square task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#1/usr/bin/python3
+
+class Square:
+    """
+
+    a class that defines a square based on Square
+    a private size attribute
+
+    """
+    __size = None
+
+    def __init___(self, size):
+        self.__size = size
