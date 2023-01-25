16c1e15174a6b0d5d752be30d1fbdea6971c4e7f
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-24 16:50:49 -0800

    2-square task

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif size size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.___size = size
