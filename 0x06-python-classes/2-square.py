0771bc3531da64e5588b93f86b746ca85ccc634e
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-24 18:43:56 -0800

    updated 2-task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
 
 class Square:
+   """Repesent a square."""
+
     def __init__(self, size=0):
-        if isinstance(size, int) is False:
-            raise TypeError("size must be an integer")
-        elif size size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.___size = size
+      """Initialize a new Square.
+      Args:
+         size (int): The size of the ne square.
+      """
+      if not isinstance(size, int):
+          raise TypeError("size must be an integer")
+      elif size < 0:
+          raise ValueError("size must be >= 0")
+      self.___size = size
