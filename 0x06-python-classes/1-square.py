1e49a9707b9117c61ab2cf14f76dd3014dc093d4
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-24 18:30:43 -0800

    updated 0-task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,11 +3,11 @@
 
 
 class Square:
-    """Represent a square. """
+    """Represent a square."""
 
     def __init___(self, size):
         """Initialize a new Square.
-        Arg:
+        Args:
             size (int): The size of the new Square
         """
         self.__size = size
