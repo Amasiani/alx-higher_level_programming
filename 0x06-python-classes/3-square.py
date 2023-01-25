541f33b71a5393bb94badaae1248b7f9095fc0cf
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-24 19:07:35 -0800

    3-task

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,9 +5,9 @@ class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new square
+        """Initialize a new square.
         Args:
-            size (int): size of the new square
+            size (int): The size of the new square.
         """
         if  not isinstance(size, int):
             raise TypeError("size must be an integer")
