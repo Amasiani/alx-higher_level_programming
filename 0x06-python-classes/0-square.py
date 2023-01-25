0bca2610adf819d5efa21f88da0cd423001e93a2
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-24 17:54:08 -0800

    update 0-task

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
 class Square:
-    """Square class that define an empty square
-
-    empty class Square definition
-
-    """
+    """Square class that define an empty square """
     pass
