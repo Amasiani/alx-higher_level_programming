2c36036df63ee876e29807128449b216f4269654
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-30 19:25:04 -0800

    1-rectangle task

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""A class that defines a reactngle."""
+"""A class that defines a rectangle."""
 
 
-class Reactangle:
-    """This represents a rectangle."""
+class Rectangle:
+    """This represents a retangle."""
 
     def __init__(self, width=0, height=0):
-        """"Initializing a new  reactnagle class.
+        """"Initializing a new  rectangle class.
         Args:
             width (int): represents the width of the rectangle.
             height (int): represents the height of the rectangle.
-        Raise:
-            TypeError: If size is not integer.
-            ValueError: If size is les than zero.
+        Raises:
+            TypeError: if size is not integer.
+            ValueError: if size is less than zero.
         """
         self.width = width
         self.height = height
