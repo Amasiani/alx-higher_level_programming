8b3e00e591301eb30d6048e4880e3824c17425b8
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-02-05 08:58:20 -0800

    100-print_python_list_info task correction

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,4 +1,4 @@
-#include <listobject.h>
+#include <Python.h>
 #include <object.h>
 #include <listobject.h>
 /**
@@ -12,7 +12,7 @@ void print_python_list_info(PyObject *p)
 	int i;
 	PyListObject *obj = (PyListObject *)p;
 
-	printf("[*] Size of the python List = %li\n", size);
+	printf("[*] Size of the Python List = %li\n", size);
 	printf("[*] Allocated = %li\n", obj->allocated);
 	for (i = 0; i < size; i++)
 		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
