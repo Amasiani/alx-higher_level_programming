f105ee9a83f0cd6b2431a3efc31d30200a382e67
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-02-06 22:31:01 -0800

    4-inherits_from task

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,11 +3,11 @@
 
 
 def inherits_form(obj, a_class):
-    """Returns a True if object is an instance of class that inherited
-       (directed or indirectedly) from the speciifed class, else false
+    """Returns True if object is an instance of class that inherited
+       (directed or indirectedly) from the speciifed class, else False
     Args:
         obj: An object
-        a_class: Class
+        a_class: A class.
     """
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
