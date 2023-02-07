766d923d5077152e0b82db3073b3a25d33e98d07
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-02-06 20:25:23 -0800

    first commit and lookup task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Inheritamce"""
+
+
+def lookup(obj):
+    """Return list of object available attributes and methods
+    Args:
+        obj: Object instance
+    """
+    return(dir(obj))
