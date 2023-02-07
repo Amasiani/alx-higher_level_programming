833d11f82a95c5ac455ee0344a3fdab57830496b
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-02-06 21:55:41 -0800

    2-is_same_class task

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Inheritance"""
+
+
+def is_same_class(obj, a_class):
+    """Returns True if the object is exactly
+       an instance of the specified classelse False
+    Args:
+        obj: An object.
+        a_class: Class(type).
+    """
+    if type(obj) is _class:
+        return True
+    else:
+        return False
