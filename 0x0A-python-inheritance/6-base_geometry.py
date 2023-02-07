187c4c52f1eb16f52c39d79b60e08803b3ae8823
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-02-06 22:43:23 -0800

    6-base_geometry task

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Inheritance"""
+
+
+class BaseGeometry:
+    """Geometry Class."""
+
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
