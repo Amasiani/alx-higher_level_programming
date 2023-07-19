b3ae300e4e8cb9a6955307baa453379df228c246
Amasiani Emeka <amasianiemeka@gmail.com>
2023-07-18 19:26:14 -0700

    1-filter_states.py task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,9 +10,10 @@ if __name__ == "__main__":
                          db=sys.argv[3], port=3306)
     
     control = db.cursor()
-    control.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id""")
+    control.execute("SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id;")
     states = control.fetchall()
+
     for state in states:
         print(state)
     control.close()
