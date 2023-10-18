aa70e95cf3f41eaa08acf227b87f545506ce023c
Emeka <amasianiemeka@gmail.com>
2023-10-18 04:09:35 +0100

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/1-body.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# sends GET request to URL and display response body
-curl -sfL "$1" -X GET
--- 0x10-python-network_0/101-post_json.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# sends a JSON POST request to URL $!, display response body
-curl -sL -H "content-type:application/json"  -d @"$2" -X POST "$1"
