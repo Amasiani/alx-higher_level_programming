e35a4911ad76fa64034970bd1d54b89899ae80d2
Amasiani <amasianiemeka@gmail.com>
2023-11-13 20:59:13 -0800

    correction

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const x = Math.floor(Number(process.argv[2]));
 if (isNaN(x)) {
-  console.log('Missing number of occurances');
+  console.log('Missing number of occurences');
 } else {
-  for (let i = 0; i < x; ++i)} {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
+}
