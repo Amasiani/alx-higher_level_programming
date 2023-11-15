2437cd4474ee5b5ff1d17d224fd3d2c97322e98d
Amasiani <amasianiemeka@gmail.com>
2023-11-14 19:39:16 -0800

    correction

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-exports.nbOccurrences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let nOccurences = 0;
   for (let i = 0; i < list.length; i++) {
     if (searchElement === list[i]) {
       nOccurences++;
     }
   }
-  return nOccurrences;
+  return nOccurences;
 };
