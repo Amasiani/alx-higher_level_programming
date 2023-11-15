e045cbe8fb4ddbb7b65f3562ca3a8c688dad0309
Amasiani <amasianiemeka@gmail.com>
2023-11-14 18:54:10 -0800

    fifth task

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+     this.height = h;
+  }
+  
+  print () {
+    for (let = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.height; j++) {
+        s += 'X';
+      }
+     console.log(s);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
