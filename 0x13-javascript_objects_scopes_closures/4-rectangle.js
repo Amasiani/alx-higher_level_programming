ef162fd72feee49931bb2d1284e9bdf596109dc4
Amasiani <amasianiemeka@gmail.com>
2023-11-15 00:27:41 -0800

    correction

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-     this.height = h;
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
   }
   
   print () {
     for (let = 0; i < this.height; i++) {
       let s = '';
-      for (let j = 0; j < this.height; j++) {
+      for (let j = 0; j < this.width; j++) {
         s += 'X';
       }
      console.log(s);
